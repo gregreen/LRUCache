08d171479d4b6f2a07cb86f75f2d328dedbc0bba
Gregory M. Green <gregorymgreen@gmail.com>
2018-07-02 23:01:50 -0700

    Initial commit. Working version of LRU cache, with cached function wrapper.

---
+++ src/lru_cache.h
@@ -0,0 +1,158 @@
+#ifndef _LRU_CACHE_H__
+#define _LRU_CACHE_H__
+
+
+#include <cstdint>
+#include <list>
+#include <unordered_map>
+#include <functional>
+
+//#include <iostream>
+
+
+template<class TKey, class TValue>
+class LRUCache {
+public:
+    LRUCache(uint32_t capacity, const TValue& empty_value);
+    ~LRUCache();
+
+    TValue get(const TKey& key);
+    void set(const TKey& key, const TValue& value);
+
+protected:
+    uint32_t capacity; // Max # of items to store.
+    TValue empty_value; // Value to return when item not in cache
+
+    // Order in which keys have been accessed. Most recent first.
+    std::list<TKey> access_order;
+
+    // Map to store (key, value) pairs.
+    std::unordered_map<TKey, TValue> cache;
+
+    // Map for quick lookup of access order.
+    std::unordered_map<TKey, typename std::list<TKey>::iterator> access_order_it;
+
+    void bring_to_front(const TKey& key);
+    void remove_lru();
+};
+
+
+template<class TKey, class TValue>
+class CachedFunction : public LRUCache<TKey,TValue> {
+public:
+    CachedFunction(std::function<TValue(const TKey&)> f, uint32_t capacity);
+    ~CachedFunction();
+    
+    TValue eval(const TKey& arg);
+    
+private:
+    std::function<TValue(const TKey&)> f;
+};
+
+
+template<class TKey, class TValue>
+CachedFunction<TKey, TValue>::CachedFunction(std::function<TValue(const TKey&)> f, uint32_t capacity)
+    : LRUCache<TKey, TValue>(capacity, TValue()), f(f)
+{}
+
+
+template<class TKey, class TValue>
+CachedFunction<TKey, TValue>::~CachedFunction() {}
+
+
+template<class TKey, class TValue>
+TValue CachedFunction<TKey, TValue>::eval(const TKey& arg) {
+    // Look up key in cache
+    auto cache_it = this->cache.find(arg);
+    if(cache_it == this->cache.end()) { // Key not found
+        //std::cout << "Evaluating f(arg)" << std::endl;
+
+        TValue value = f(arg);
+        this->set(arg, value);
+        return value;
+    } else { // Key found
+        //std::cout << "Using cached result of f(arg)" << std::endl;
+
+        // Update access order
+        this->bring_to_front(arg);
+        // Return cached value
+        return cache_it->second;
+    }
+}
+
+
+template<class TKey, class TValue>
+LRUCache<TKey, TValue>::LRUCache(uint32_t capacity, const TValue& empty_value)
+    : capacity(capacity), empty_value(empty_value)
+{}
+
+
+template<class TKey, class TValue>
+LRUCache<TKey, TValue>::~LRUCache() {}
+
+
+template<class TKey, class TValue>
+TValue LRUCache<TKey, TValue>::get(const TKey& key) {
+    // Look up key in cache
+    auto cache_it = cache.find(key);
+    if(cache_it == cache.end()) { // Key not found
+        return empty_value;
+    } else { // Key found
+        // Update access order
+        bring_to_front(key);
+        // Return cached value
+        return cache_it->second;
+    }
+}
+
+
+template<class TKey, class TValue>
+void LRUCache<TKey, TValue>::set(const TKey& key, const TValue& value) {
+    // Look up key in cache
+    auto cache_it = cache.find(key);
+    if(cache_it == cache.end()) { // Key not in cache
+        // Add (key, value) pair to cache
+        cache_it = cache.insert({key, value}).first;
+        // Insert key into access-order list
+        access_order.push_front(key);
+        // Update access-order lookup
+        access_order_it.insert({key, access_order.begin()});
+        // If over capacity, remove least-recently-used key
+        if(cache.size() > capacity) {
+            remove_lru();
+        }
+    } else { // Key found
+        // Bring key to front of access order list
+        bring_to_front(key);
+        // Update value
+        cache_it->second = value;
+    }
+}
+
+
+template<class TKey, class TValue>
+void LRUCache<TKey, TValue>::remove_lru() {
+    // Look up key of least-recently-used element
+    const TKey& key = access_order.back();
+    // Erase key from cache and access-order-iterator lookup
+    cache.erase(key);
+    access_order_it.erase(key);
+    // Erase key from access-order list
+    access_order.pop_back();
+}
+
+
+template<class TKey, class TValue>
+void LRUCache<TKey, TValue>::bring_to_front(const TKey& key) {
+    auto ao_it = access_order_it.find(key); // Assumed to be found
+    if(ao_it->second != access_order.begin()) {
+        // Move key to front of access-order list
+        access_order.erase(ao_it->second);
+        access_order.push_front(key);
+        // Update location of key in access-order-iterator lookup
+        ao_it->second = access_order.begin();
+    }
+}
+
+
+#endif // _LRU_CACHE_H__
---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+
+#include "lru_cache.h"
+#include <iostream>
+#include <limits>
+
+
+int main(int argc, char* argv[]) {
+    uint32_t capacity = 4;
+    double empty_value = std::numeric_limits<double>::quiet_NaN();
+    LRUCache<int, double> cache(capacity, empty_value);
+
+    cache.set(0, 1.);
+    cache.set(1, 2.);
+    cache.set(2, 3.);
+    cache.set(3, 4.);
+    cache.set(5, 6.);
+    
+    std::cout << cache.get(0) << std::endl;
+    std::cout << cache.get(1) << std::endl;
+    std::cout << cache.get(2) << std::endl;
+    std::cout << cache.get(3) << std::endl;
+    std::cout << cache.get(4) << std::endl;
+    std::cout << cache.get(5) << std::endl;
+
+
+    CachedFunction<int, double> f(
+        [](int x) -> double {
+            return x*x;
+        },
+        capacity
+    );
+
+    for(int i=-5; i<=5; i++) {
+        std::cout << f.eval(i) << std::endl;
+    }
+    for(int i=5; i>=-5; i--) {
+        std::cout << f.eval(i) << std::endl;
+    }
+
+    return 0;
+}
+
